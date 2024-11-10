c73bd83350cab607bbaefc3e5bd64926df9bce81
Doi_Trong_Canh <doicanh226@gmail.com>
2024-11-10 22:30:30 +0700

    reverse_iterator.cpp

--- reverse_iterator.cpp
+++ reverse_iterator.cpp
@@ -9,22 +9,18 @@ class vector_reverse_iterator
 public:
     vector_reverse_iterator() { curr = nullptr; }
     vector_reverse_iterator(T *c = nullptr) { curr = c; }
-    vector_reverse_iterator<T> &operator=(const vector_reverse_iterator<T> &t)
-    {
+    vector_reverse_iterator<T> &operator=(const vector_reverse_iterator<T> &t){
         this->curr = t.curr;
         return *this;
     }
-    bool operator!=(const vector_reverse_iterator<T> &t)
-    {
+    bool operator!=(const vector_reverse_iterator<T> &t){
         return this->curr != t.curr;
     }
-    vector_reverse_iterator<T> &operator++()
-    {
+    vector_reverse_iterator<T> &operator++(){
         curr = curr - 1;
         return *this;
     }
-    vector_reverse_iterator<T> &operator++(int)
-    {
+    vector_reverse_iterator<T> &operator++(int){
         vector_reverse_iterator<T> temp = *this;
         this->curr = this->curr - 1;
         return temp;
